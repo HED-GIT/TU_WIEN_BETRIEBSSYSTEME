55734dd59237dfb7a15723915a857ac2e3876387
dallinger hannes <dallinger.hannes@gmail.com>
2020-09-03 16:38:12 +0200

    updated myexpand

--- 1a/myexpand/myexpand.c
+++ 1a/myexpand/myexpand.c
@@ -2,14 +2,40 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-#define USAGE() {fprintf(stderr,"%s\t[USAGE]\t %s [-t tabstop] [file...]\n",fileName,fileName);exit(EXIT_FAILURE);}
-#define ERROR_EXIT(...) { fprintf(stderr, "%s\t[ERROR] " __VA_ARGS__"\n",fileName); exit(EXIT_FAILURE); }
-#define SUCCESS_EXIT() {exit(EXIT_SUCCESS);}
+// macro used to print usage
+// do while is not strictly needed but prevents errors when using the macro in some specific situations (but not in this program)
+#define USAGE()																	\
+	do{																			\
+		fprintf(stdout,"USAGE: %s [-s] [-i] [-o outfile] [file...]", name);		\
+		exit(EXIT_FAILURE);														\
+	} while(0)
 
-int tabstop = 8;
-char * fileName;
 
-void readFile (FILE * file){
+// macro used to print errors, program still continues on
+// the ... let's you give arguments to the macro which will automaticly be written at the position of __VA_ARGS__
+#define ERROR_MSG(...)											\
+	do{ 														\
+		fprintf(stderr, "%s ERROR:\t", name); 					\
+		fprintf(stderr, __VA_ARGS__); 							\
+		fprintf(stderr, "\n");									\
+	} while(0)
+
+
+// macro used to exit the program and automaticly print an error message
+#define ERROR_EXIT(...)											\
+	do{ 														\
+		ERROR_MSG(__VA_ARGS__);									\
+		exit(EXIT_FAILURE); 									\
+	} while(0)
+
+
+char * name = "myexpand";	// holds name of the program, is hardcoded since argv[0] doesn't have to hold the correct name
+							// for example it could be set wrong when the program is executed using exec (man exec)
+							// but that also means that it doesn't change when you rename the program
+							// under linux you could figure out the correct name using 'readlink("/proc/self/exe", char * buf, size_t bufsiz);' but i can't be bothered to do it like this
+
+
+void readFile(FILE * file, int tabstop){
 	char readChar;
 	int writePos = 0;
 	while((readChar = fgetc(file))!=EOF){
@@ -27,18 +53,17 @@ void readFile (FILE * file){
 			writePos=0;
 		}
 	}
+	if(!feof(file)){
+		ERROR_EXIT("couldn't read file to end");
+	}
 }
 
 int main(int argc, char ** argv){
-	fileName = argv[0];
-	
-	int arguments = 0;
-	
+	int tabstop = 8;
 	int opt;
 	while((opt=getopt(argc,argv,"t:"))!=-1){
 		switch(opt){
 			case 't':
-				arguments++;
 				tabstop = (int) strtol(optarg, (char **)NULL, 10);
 				break;
 			default:
@@ -49,22 +74,19 @@ int main(int argc, char ** argv){
 	if(tabstop <= 0){
 		ERROR_EXIT("value of -t is invalid");
 	}
-	if(arguments > 1){
-		USAGE();
-	}
-	
-	if(arguments * 2 + 1 == argc){
-		readFile(stdin);
-		SUCCESS_EXIT();
+
+	if(optind == argc){
+		readFile(stdin, tabstop);
+		exit(EXIT_SUCCESS);
 	} else{
-		for(int i = arguments * 2 + 1; i < argc; i++){
-			FILE * input = fopen(argv[i],"r");
+		for(int i = optind; i < argc; i++){
+			FILE * input = fopen(argv[i], "r");
 			if(input == NULL){
 				ERROR_EXIT("unable to open file");
 			}
-			readFile(input);
+			readFile(input, tabstop);
 			fclose(input);
 		}
-		SUCCESS_EXIT();
+		exit(EXIT_SUCCESS);
 	}
 }
