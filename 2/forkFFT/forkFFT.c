fa9b52ff07ee86ecf81cee136989b6776ace0dd9
dallinger hannes <dallinger.hannes@gmail.com>
2020-12-21 09:36:04 +0100

    refactoring forkFFT

--- 2/forkFFT/forkFFT.c
+++ 2/forkFFT/forkFFT.c
@@ -42,19 +42,25 @@ typedef struct ComplexNumber {
 *@param the complexNumbers to add
 *@return new complexNumber with the added value
 */
-static void add(ComplexNumber * x, ComplexNumber * y) {
-    x->imaginary += y->imaginary;
-    x->real += y->real;
+static ComplexNumber add(const ComplexNumber * x, const ComplexNumber * y) {
+    ComplexNumber ret = { 
+        x->imaginary + y->imaginary,
+        x->real + y->real
+    };
+    return ret;
 }
 
 /**
 *@brief subtracts two complexNumbers
-*@param the complexNumbers to subtract
+*@param the complexNumbers to subtract 
 *@return new complexNumber with the subtracted value
 */
-static void subtract(ComplexNumber * x, ComplexNumber * y) {
-    x->imaginary -= y->imaginary;
-    x->real -= y->real;
+static ComplexNumber subtract(const ComplexNumber * x, const ComplexNumber * y) {
+    ComplexNumber ret = { 
+        x->imaginary - y->imaginary,
+        x->real - y->real
+    };
+    return ret;
 }
 
 /**
@@ -62,13 +68,12 @@ static void subtract(ComplexNumber * x, ComplexNumber * y) {
 *@param the complexNumbers to multiply
 *@return new complexNumber with the multiplied value
 */
-static void multiply(ComplexNumber * x, ComplexNumber * y) {
-    ComplexNumber z;
-	z.real=x->real;
-	z.imaginary=x->imaginary;
-	
-    x->imaginary = ((z.real) * (y->imaginary) + (z.imaginary) * (y->real));
-    x->real = (z.real) * (y->real) - (z.imaginary) * (y->imaginary);
+static ComplexNumber multiply(const ComplexNumber * x, const ComplexNumber * y) {
+    ComplexNumber ret = {
+        ((x->real) * (y->imaginary) + (x->imaginary) * (y->real)),
+        (x->real) * (y->real) - (x->imaginary) * (y->imaginary)
+    };
+    return ret;
 }
 
 /**
@@ -112,6 +117,31 @@ static void dupNeededPipes(int pipeAmount, int pipes[pipeAmount][2], int neededR
 	}
 }
 
+
+static int readInput(ComplexNumber * numbers){
+    char * line = NULL;	    //read string from stdin
+    ssize_t nread;	        //errorvalue for getline
+    size_t len = 0;	        //length is irrelivant because line=null
+    char * end;		        //pointer to position after strtof
+    int counter = 0;
+    while ((nread = getline( & line, & len, stdin)) != -1) {
+
+        numbers[counter].real = strtof(line, & end);
+        checkForNumber(line);
+        if (!( * end != '\n' || * end != ' ')) {
+            ERROR_EXIT("invalid number");
+        }
+        numbers[counter].imaginary = strtof(end, & end);
+
+        if (!( * end != '\n' || * end != ' ')) {
+            ERROR_EXIT("invalid number");
+        }
+        counter++;
+    }
+    freeRegex();
+    return counter;
+}
+
 /**
 *@brief Programm entry point
 *@detail reads from stdin, splits the values and gives them to two child process
@@ -125,44 +155,29 @@ int main(int argc, char * argv[]) {
 	regexInit();
 	
 	ComplexNumber readNumbers[MAXLENGTH];	//saves all numbers read from stdin
-    int counter = 0;					//saves amount of numbers read from stdin
+					//saves amount of numbers read from stdin
 
     if (argc != 1) {
         USAGE();
     }
 
-    char * line = NULL;	//read string from stdin
-    ssize_t nread;	//errorvalue for getline
-    size_t len = 0;	//length is irrelivant because line=null
-    char * end;		//pointer to position after strtof
-    while ((nread = getline( & line, & len, stdin)) != -1) {
+    int numberAmount = readInput(readNumbers);
 
-        readNumbers[counter].real = strtof(line, & end);
-        checkForNumber(line);
-        if (!( * end != '\n' || * end != ' ')) {
-            ERROR_EXIT("test");
-        }
-        readNumbers[counter].imaginary = strtof(end, & end);
 
-        if (!( * end != '\n' || * end != ' ')) {
-            ERROR_EXIT("test");
-        }
-        counter++;
-    }
 
-    if (counter == 1) {
+    if (numberAmount == 1) {
         fprintf(stdout, "%f %f *i\n", readNumbers[0].real, readNumbers[0].imaginary);
         SUCCESS_EXIT();
     }
-    if (counter % 2 != 0) {
+    if (numberAmount % 2 != 0) {
         ERROR_EXIT("amount of input must be 2^n!");
     }
 
-    ComplexNumber ** newNumbers = malloc(sizeof(ComplexNumber)*counter);	//saves new calculated numbers
+    ComplexNumber * newNumbers = malloc(sizeof(ComplexNumber)*numberAmount);	//saves new calculated numbers
 
 	int pipes[4][2];
 
-    if (pipe(pipes[PIPE_E_WRITE]) == -1 || pipe(pipes[PIPE_E_READ]) == -1 || pipe(pipes[PIPE_O_WRITE]) == -1 || pipe(pipes[PIPE_O_READ]) == -1) {
+        if (pipe(pipes[PIPE_E_WRITE]) == -1 || pipe(pipes[PIPE_E_READ]) == -1 || pipe(pipes[PIPE_O_WRITE]) == -1 || pipe(pipes[PIPE_O_READ]) == -1) {
         ERROR_EXIT("Pipe-Error");
     }
 
@@ -170,13 +185,13 @@ int main(int argc, char * argv[]) {
     if((p1 = fork())==-1){ERROR_EXIT("fork-Error");}				
     if (p1 == 0) {
 		dupNeededPipes(4,pipes,PIPE_E_READ,PIPE_E_WRITE);
-        if(execlp("./forkFFT", argv[0], NULL)==-1){ERROR_EXIT("execlp-Error");}
+        if(execlp("./forkFFT", fileName, NULL)==-1){ERROR_EXIT("execlp-Error");}
     }
     int p2;					//second child process
     if((p2 = fork())==-1){ERROR_EXIT("fork-Error");}	
     if (p2 == 0) {
 		dupNeededPipes(4, pipes,PIPE_O_READ,PIPE_O_WRITE);
-        if(execlp("./forkFFT", argv[0], NULL)==-1){ERROR_EXIT("execlp-Error");}
+        if(execlp("./forkFFT", fileName, NULL)==-1){ERROR_EXIT("execlp-Error");}
     }
     close(pipes[PIPE_E_WRITE][READ]);
     close(pipes[PIPE_O_WRITE][READ]);
@@ -184,13 +199,13 @@ int main(int argc, char * argv[]) {
     close(pipes[PIPE_O_READ][WRITE]);
     char floatToString[MAXLENGTH];		//string that will be writen to the pipe
 
-    for (int i = 0; i < counter; i++) {
+    for (int i = 0; i < numberAmount; i++) {
 
         snprintf(floatToString, sizeof(floatToString), "%.5f %.5f*i\n", readNumbers[i].real, readNumbers[i].imaginary);
         if (i % 2 == 1)
-            write(pipes[PIPE_E_WRITE][WRITE], floatToString, (strlen(floatToString)));
-        else
             write(pipes[PIPE_O_WRITE][WRITE], floatToString, (strlen(floatToString)));
+        else
+            write(pipes[PIPE_E_WRITE][WRITE], floatToString, (strlen(floatToString)));
     }
     close(pipes[PIPE_E_WRITE][WRITE]);
     close(pipes[PIPE_O_WRITE][WRITE]);
@@ -214,12 +229,12 @@ int main(int argc, char * argv[]) {
     FILE * fileE = fdopen(pipes[PIPE_E_READ][READ], "r");	//opens pipe as file
     FILE * fileO = fdopen(pipes[PIPE_O_READ][READ], "r");	//opens pipe as file
 
-    for (int k = 0; k < counter / 2; k++) {
+    for (int k = 0; k < numberAmount / 2; k++) {
         ComplexNumber e; 		//saves read E-value
-        ComplexNumber o;			//saves read O-value
-	//E is actually O and O is actually E (writes wrong values to wrong pipe)
-        ComplexNumber  *new1 = malloc(sizeof(ComplexNumber));		//saves new calculated value
-        ComplexNumber * new2 = malloc(sizeof(ComplexNumber));		//saves new calculated value
+        ComplexNumber o;		//saves read O-value
+
+        ComplexNumber new1;		//saves new calculated value
+        ComplexNumber new2;		//saves new calculated value
         getline( & eline, & length, fileE);
         e.real = strtof(eline, & eline);
         e.imaginary = strtof(eline, & eline);
@@ -228,24 +243,22 @@ int main(int argc, char * argv[]) {
         o.real = strtof(oline, & oline);
         o.imaginary = strtof(oline, & oline);
 
-        new1->real = cos((-((2 * PI) / counter)) * k);
-        new1->imaginary = sin((-((2 * PI) / counter)) * k);
-        multiply(new1, &e);
-		add(new1,&o);
+        new1.real = cos((-((2 * PI) / numberAmount)) * k);
+        new1.imaginary = sin((-((2 * PI) / numberAmount)) * k);
+        new1 = multiply(&new1, &o);
+		new1 = add(&new1,&e);
         newNumbers[k] = new1;
 
-        new2->real = cos((-((2 * PI) / counter)) * (k));
-        new2->imaginary = sin((-((2 * PI) / counter)) * (k));
-        multiply(new2, &e);
-        subtract(new2, &o);
-        newNumbers[k + (counter / 2)] = new2;
+        new2.real = cos((-((2 * PI) / numberAmount)) * (k));
+        new2.imaginary = sin((-((2 * PI) / numberAmount)) * (k));
+        new2 = multiply(&new2, &o);
+        new2 = subtract(&e, &new2);
+        newNumbers[k + (numberAmount / 2)] = new2;
     }
-    for (int i = 0; i < counter; i++) {
-        fprintf(stdout, "%.6f %.6f *i\n", newNumbers[i]->real, newNumbers[i]->imaginary);
-		free(newNumbers[i]);
+    for (int i = 0; i < numberAmount; i++) {
+        fprintf(stdout, "%.6f %.6f *i\n", newNumbers[i].real, newNumbers[i].imaginary);
 	}
 	
-	freeRegex();
 	free(newNumbers);
     SUCCESS_EXIT();
 }
